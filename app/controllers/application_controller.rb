3b0bcfd062063c678c72cebefdc03a3d65f01617
Nat14 <naryucha14@gmail.com>
2016-03-26 20:58:56 -0700

    updated CSRF token error in heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,16 @@ class ApplicationController < ActionController::Base
 
   # protect_from_forgery with: :exception
 
-  # change to this 
+  # nat - change to this to fix the issue of "Can't verify CSRF token authenticity"
   protect_from_forgery with: :null_session
 
+  # Nat added - http://stackoverflow.com/questions/30204321/rails-cant-verify-csrf-token-authenticity
+  # API POST REGUEST ALLOW CROSS DOMAIN
+    before_filter :cor
+    def cor
+      headers["Access-Control-Allow-Origin"]  = "*"
+      headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+      headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+      head(:ok) if request.request_method == "OPTIONS"
+    end
 end
